5ebd7ff5b0355fec537cf0c671e0e5f96f52905e
mao <1126924038@qq.com>
2016-11-22 18:49:36 +0800

    添加上传一个文件功能

--- app/src/main/java/com/mao/travelapp/MainActivity.java
+++ app/src/main/java/com/mao/travelapp/MainActivity.java
@@ -1,18 +1,10 @@
 package com.mao.travelapp;
 
-import android.content.SyncStatusObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.mao.travelapp.bean.User;
-import com.mao.travelapp.sdk.BaseObject;
-import com.mao.travelapp.sdk.CommonCallback;
-import com.mao.travelapp.sdk.QueryCallback;
-
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.mao.travelapp.sdk.UploadFileCallback;
+import com.mao.travelapp.sdk.FileHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +13,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //添加测试
-//        User user = new User();
-//        user.setUsername("gou");
-//        user.setPassword("123456");
-//        user.save(new CommonCallback() {
+//        //添加测试
+////        User user = new User();
+////        user.setUsername("gou");
+////        user.setPassword("123456");
+////        user.save(new CommonDBCallback() {
+////            @Override
+////            public void onSuccess(int affectedRowCount) {
+////                System.out.println("affectedRowCount:" + affectedRowCount);
+////            }
+////
+////            @Override
+////            public void onFail(String error) {
+////                System.out.println("error:" + error);
+////            }
+////        });
+//
+//        //更新测试
+//
+//        //删除测试
+//        Map<String, String> where = new HashMap<String, String>();
+//        //where.put("id", "1");
+//        BaseObject.delete(where, User.class, new CommonDBCallback() {
 //            @Override
 //            public void onSuccess(int affectedRowCount) {
 //                System.out.println("affectedRowCount:" + affectedRowCount);
@@ -36,16 +45,34 @@ public class MainActivity extends AppCompatActivity {
 //                System.out.println("error:" + error);
 //            }
 //        });
+//
+//        //查询测试
+//        /*Map<String, String> where = new HashMap<String, String>();
+//        BaseObject.query(where, User.class, new QueryCallback<User>(){
+//
+//            @Override
+//            public void onSuccess(List<User> list) {
+//                for(User u : list) {
+//                    System.out.println(u.getUsername() + " " + u.getPassword());
+//                }
+//            }
+//
+//            @Override
+//            public void onFail(String error) {
+//                System.out.println(error);
+//            }
+//        });*/
 
-        //更新测试
 
-        //删除测试
-        Map<String, String> where = new HashMap<String, String>();
-        //where.put("id", "1");
-        BaseObject.delete(where, User.class, new CommonCallback() {
+
+
+        //文件测试
+        String path = "/sdcard/123.jpg";
+        FileHelper.uploadOne(path, new UploadFileCallback() {
+
             @Override
-            public void onSuccess(int affectedRowCount) {
-                System.out.println("affectedRowCount:" + affectedRowCount);
+            public void onSuccess(String url) {
+                System.out.println("url:" + url);
             }
 
             @Override
@@ -53,25 +80,6 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("error:" + error);
             }
         });
-
-        //查询测试
-        /*Map<String, String> where = new HashMap<String, String>();
-        BaseObject.query(where, User.class, new QueryCallback<User>(){
-
-            @Override
-            public void onSuccess(List<User> list) {
-                for(User u : list) {
-                    System.out.println(u.getUsername() + " " + u.getPassword());
-                }
-            }
-
-            @Override
-            public void onFail(String error) {
-                System.out.println(error);
-            }
-        });*/
-
-
 
     }
 
